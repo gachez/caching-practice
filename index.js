e513e17dca91891af96e8b9e14f3af00dff89e17
gachez <briangach98@gmail.com>
2020-10-10 22:23:38 +0300

    caching requests using flatCache

--- index.js
+++ index.js
@@ -4,31 +4,53 @@ const PORT = process.env.PORT || 3128;
 const app = express();
 
 //memory-cache
-const cache = require('memory-cache');
+// const cache = require('memory-cache');
 
-//configure cache middleware
-let memCache = new cache.Cache();
-// memcache function middleware
-let cacheMiddleware = (duration) => {
-    return (req, res, next) => {
-        let key = '__express__' + req.originalUrl || req.originalUrl
-        let cacheContent = memCache.get(key);
-        if(cacheContent){
-            res.send(cacheContent);
-            return
-        } else {
-            res.sendResponse = res.send
-            res.send = (body) => {
-                memCache.put(key,body,duration*1000);
-                res.sendResponse(body)
-            }
-            next()
+// //configure cache middleware
+// let memCache = new cache.Cache();
+// // memcache function middleware
+// let cacheMiddleware = (duration) => {
+//     return (req, res, next) => {
+//         let key = '__express__' + req.originalUrl || req.originalUrl
+//         let cacheContent = memCache.get(key);
+//         if(cacheContent){
+//             res.send(cacheContent);
+//             return
+//         } else {
+//             res.sendResponse = res.send
+//             res.send = (body) => {
+//                 memCache.put(key,body,duration*1000);
+//                 res.sendResponse(body)
+//             }
+//             next()
+//         }
+//     }
+// }
+
+//flat-cache
+const flatCache = require('flat-cache');
+
+// load new cache
+let cache = flatCache.load('productsCache');
+
+//flat-cache middleware
+let flatCacheMiddleware = (req,res,next) => {
+    let key = '__express__' + req.originalUrl || req.originalUrl
+    let cacheContent = cache.getKey(key);
+    if(cacheContent){
+        res.send( cacheContent );
+    } else{
+        res.sendResponse = res.send
+        res.send = (body) => {
+            cache.setKey(key, body);
+            cache.save();
+            res.sendResponse(body)
         }
+        next()
     }
 }
 
-
-app.get('/products', cacheMiddleware(30), function(req, res){
+app.get('/products', flatCacheMiddleware, function(req, res){
     setTimeout(() => {
       let db = new sqlite3.Database('./NodeInventory.db');
       let sql = `SELECT * FROM products`;
