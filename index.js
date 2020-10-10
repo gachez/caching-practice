ba4ca46539cc5067b5d10d2045122c2b84223d23
gachez <briangach98@gmail.com>
2020-10-10 22:07:29 +0300

    caching express requests using memory-cache

--- index.js
+++ index.js
@@ -3,8 +3,32 @@ const sqlite3 = require('sqlite3').verbose();
 const PORT = process.env.PORT || 3128;
 const app = express();
 
+//memory-cache
+const cache = require('memory-cache');
 
-app.get('/products',  function(req, res){
+//configure cache middleware
+let memCache = new cache.Cache();
+// memcache function middleware
+let cacheMiddleware = (duration) => {
+    return (req, res, next) => {
+        let key = '__express__' + req.originalUrl || req.originalUrl
+        let cacheContent = memCache.get(key);
+        if(cacheContent){
+            res.send(cacheContent);
+            return
+        } else {
+            res.sendResponse = res.send
+            res.send = (body) => {
+                memCache.put(key,body,duration*1000);
+                res.sendResponse(body)
+            }
+            next()
+        }
+    }
+}
+
+
+app.get('/products', cacheMiddleware(30), function(req, res){
     setTimeout(() => {
       let db = new sqlite3.Database('./NodeInventory.db');
       let sql = `SELECT * FROM products`;
