27a9a15e99cac3cac50ff2d6d10b82d81ca05dd9
gachez <briangach98@gmail.com>
2020-10-10 21:30:47 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const PORT = process.env.PORT || 3128;
+const app = express();
+
+
+app.get('/products',  function(req, res){
+    setTimeout(() => {
+      let db = new sqlite3.Database('./NodeInventory.db');
+      let sql = `SELECT * FROM products`;
+
+      db.all(sql, [], (err, rows) => {
+          if (err) {
+              throw err;
+          }
+          db.close();
+          res.send( rows );
+      });
+      // this was wrapped in a setTimeout function to intentionally simulate a slow 
+      // request
+    }, 3000);
+});
